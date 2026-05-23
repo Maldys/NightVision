7fc0833ecd51c75aefd699545e21d43cbc1b419a
Matyas Malda <matyas.malecek@email.cz>
2026-05-23 11:23:06 +0200

    removed gpio_test and ir from context

--- src/context.py
+++ src/context.py
@@ -8,7 +8,6 @@ from camera_service import Camera_Service
 from cross_params import Crosshair_params
 from context_saver import Context_saver
 from mode import Mode
-from gpiozero import PWMLED
 from ads1115 import ADS1115Reader
 
 
@@ -25,9 +24,7 @@ class Context:
     camera: Camera_Service = field(default_factory=Camera_Service)
     cross_params: Crosshair_params = field(default_factory=lambda: [Crosshair_params() for _ in range(5)])
     context_saver: Context_saver = field(init=False)
-    ir_level: float = 0.0
-    pwm_ir: PWMLED = field(init=False) 
-    ir_x_zoom = False #False = ir, True = zoom
+ 
     ads: ADS1115Reader = field(default=ADS1115Reader)
     
 
@@ -35,7 +32,7 @@ class Context:
     def __post_init__(self):
         self.camera.attach(self)
         self.context_saver = Context_saver(self)
-        self.pwm_ir = PWMLED(18, initial_value=self.ir_level)
+
  
         
     
