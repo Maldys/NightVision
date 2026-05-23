023366ff95f97e344fb23e4c06682e86a1feedd1
Matyas Malda <matyas.malecek@email.cz>
2026-05-23 11:18:52 +0200

    no_ir full init

---
+++ src/cross_params.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from cross_type import Cross_type
+
+@dataclass
+class Crosshair_params:
+    cross_type: Cross_type = Cross_type.DOT
+    color: tuple = (255, 0, 0) #(R,G,B)
+    thickness: int = 2
+    scale: float = 1.0
+    size: int = 100
+    x_offset: int = 0
+    y_offset: int = 0
+    
